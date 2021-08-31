8784fe0131201bbf6d4d0c13961cc5c4def814de
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:41:52 +0530

    Factorial

---
+++ Factorialnumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorialnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i, fact = 1, number;
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+        }
+    }
+}
